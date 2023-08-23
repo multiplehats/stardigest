b43eff964ffbd4d2556cac2a66f5d153b715dd5d
Jaeden <ch.schwartze@gmail.com>
2023-08-23 14:41:57 +0200

    chore: remove redundant files

--- src/lib/services/auth/types.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod';
-
-// Name has a default value just to display something in the form.
-const schema = z.object({
-	name: z.string().default('Hello world!'),
-	email: z.string().email()
-});
