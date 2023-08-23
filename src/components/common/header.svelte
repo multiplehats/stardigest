0779af36a342407dc87d3fe10cd0bd4cd5b94383
Jaeden <ch.schwartze@gmail.com>
2023-08-23 17:31:56 +0200

    fix: type error

--- src/components/common/header.svelte
+++ src/components/common/header.svelte
@@ -77,11 +77,7 @@
 						stroke="currentColor"
 						aria-hidden="true"
 					>
-						<path
-							stroke-linecap="round"
-							strokYe-linejoin="round"
-							d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-						/>
+						<path stroke-linecap="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
 					</svg>
 					<!-- Menu open: "block", Menu closed: "hidden" -->
 					<svg
