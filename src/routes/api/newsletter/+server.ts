3adf831a4195abca9ab2e5fbec0616a6b75278e8
Jaeden <ch.schwartze@gmail.com>
2023-08-24 10:03:11 +0200

    chore: update responses -> 200

--- src/routes/api/newsletter/+server.ts
+++ src/routes/api/newsletter/+server.ts
@@ -31,48 +31,54 @@ export const POST: RequestHandler = async ({ request }) => {
 		return new Response();
 	}
 
-	const user = await db.query.users.findFirst({
-		where: eq(schema.users.id, data.data.userId)
-	});
+	try {
+		const user = await db.query.users.findFirst({
+			where: eq(schema.users.id, data.data.userId)
+		});
 
-	if (!user) {
-		console.error('User not found');
+		if (!user) {
+			console.error('User not found');
 
-		return new Response('User not found', { status: 404 });
+			return new Response('User not found', { status: 200 });
+		}
+
+		if (!user.github_access_token) {
+			console.error('User has no github access token');
+
+			return new Response('User has no github access token', { status: 200 });
+		}
+
+		const accessToken = decryptAccessToken(user.github_access_token);
+		const githubService = new GithubService(accessToken, user.id);
+		const stars = await githubService.getUniqueRandomStars();
+
+		if (!stars || stars.length === 0) {
+			console.error('User has no stars');
+
+			return new Response('User has no stars', { status: 200 });
+		}
+
+		const html = await renderAsync(
+			React.createElement(Email, {
+				username: user.github_username ?? user.name ?? 'you',
+				starredRepos: stars
+			} satisfies {
+				username: string;
+				starredRepos: UserStar[];
+			})
+		);
+
+		await sendgrid.sendMail({
+			to: user.email,
+			subject: 'Your daily dose of stars',
+			html: html,
+			text: 'Your daily dose of stars'
+		});
+
+		return new Response();
+	} catch (error) {
+		console.error(error);
+
+		return new Response(error.message, { status: 200 });
 	}
-
-	if (!user.github_access_token) {
-		console.error('User has no github access token');
-
-		return new Response('User has no github access token', { status: 404 });
-	}
-
-	const accessToken = decryptAccessToken(user.github_access_token);
-	const githubService = new GithubService(accessToken, user.id);
-	const stars = await githubService.getUniqueRandomStars();
-
-	if (!stars || stars.length === 0) {
-		console.error('User has no stars');
-
-		return new Response('User has no stars', { status: 404 });
-	}
-
-	const html = await renderAsync(
-		React.createElement(Email, {
-			username: user.github_username ?? user.name ?? 'you',
-			starredRepos: stars
-		} satisfies {
-			username: string;
-			starredRepos: UserStar[];
-		})
-	);
-
-	await sendgrid.sendMail({
-		to: user.email,
-		subject: 'Your daily dose of stars',
-		html: html,
-		text: 'Your daily dose of stars'
-	});
-
-	return new Response();
 };
