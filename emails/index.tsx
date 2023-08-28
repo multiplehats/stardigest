84bdbf6393231cbf2ba00e648f7bfcdb594840e6
Jaeden <ch.schwartze@gmail.com>
2023-08-28 23:14:25 +0200

    style: improve email

--- emails/index.tsx
+++ emails/index.tsx
@@ -29,7 +29,7 @@ export const GithubStarredRepoEmail = ({
 }: GithubStarredRepoEmailProps) => {
 	const previewText = `Most recent Github repos ${username} has starred`;
 	const now = Date.now();
-	const ogHash = now.toString(36);
+	const ogHash = process.env.NODE_ENV === 'production' ? now : '123456789';
 
 	return (
 		<Html>
@@ -39,13 +39,15 @@ export const GithubStarredRepoEmail = ({
 				<Body className="bg-[#121212] my-auto mx-auto font-sans">
 					<Container className="my-[40px] mx-auto p-[20px] max-w-[580px] w-full">
 						<Section>
-							<Img
-								src="https://www.stardigest.app/logo-email.png"
-								className="mx-auto mb-[30px]"
-								width="170"
-								height="50"
-								alt="Logo"
-							/>
+							<a href="https://www.stardigest.app" target="_blank" rel="noreferrer">
+								<Img
+									src="https://www.stardigest.app/logo-email.png"
+									className="mx-auto mb-[30px]"
+									width="190"
+									height="41"
+									alt="Logo"
+								/>
+							</a>
 
 							<Heading className="text-white text-[18px] font-bold text-center p-0 mb-[30px] mt-[18px] mx-0">
 								Here's your star digest {username}
@@ -54,36 +56,40 @@ export const GithubStarredRepoEmail = ({
 							{starredRepos.map(({ id, owner, repo, description, stars, url }) => (
 								<Section
 									key={id}
-									className="bg-[#1e1e1e] border-solid rounded-md border border-[#424241] w-auto px-4 py-3 mb-[30px]"
+									className="bg-[#1e1e1e] border-solid rounded-md border border-[#424241] w-auto px-4 py-6 mb-[30px]"
 								>
-									<Row className="mx-auto mb-[10px] align-top">
-										<Column align="left" className="align-top">
-											<Heading className="text-white text-[22px] mt-[0px] mb-[0px]">
-												{owner}/{repo}
-											</Heading>
-
-											<Text className="text-gray-400 max-w-[400px] text-[14px] mt-[0px] mb-[0px] leading-[24px]">
-												{description}
-											</Text>
+									<Row className="mx-auto mb-[10px] align-center">
+										<Column align="left" className="align-center">
+											<a href={url} target="_blank" rel="noreferrer">
+												<Heading className="text-white text-[22px] mt-[0px] mb-[0px]">
+													{owner}/{repo}
+												</Heading>
+											</a>
 										</Column>
 
-										<Column align="right" className="align-top">
+										<Column align="right" className="align-center">
 											<Text className="text-gray-400 text-[14px] mt-[0px] mb-[0px]">
 												⭐️ {stars}
 											</Text>
 										</Column>
 									</Row>
 
-									<Img
-										width={480}
-										className="w-full h-auto rounded-sm overflow-hidden mb-[20px]"
-										src={`https://opengraph.githubassets.com/${ogHash}/${owner}/${repo}`}
-									/>
+									<a href={url} target="_blank" rel="noreferrer">
+										<Img
+											width={480}
+											className="w-full h-auto rounded-sm overflow-hidden mb-[20px]"
+											src={`https://opengraph.githubassets.com/${ogHash}/${owner}/${repo}`}
+										/>
+									</a>
+
+									<Text className="text-gray-400 text-[18px] mt-[0px] mb-[15px] leading-[26px]">
+										{description}
+									</Text>
 
 									<Button
 										pX={16}
 										pY={8}
-										className="bg-[#d3ac5b] rounded text-white text-[12px] font-semibold no-underline text-center"
+										className="bg-[#d3ac5b] rounded text-[#121212] text-[14px] font-semibold no-underline text-center"
 										href={url}
 									>
 										Hop back in
