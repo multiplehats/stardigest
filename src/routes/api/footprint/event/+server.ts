a59873c8c6f49233041ff817d42a1abf24682b5b
Jaeden <ch.schwartze@gmail.com>
2023-08-29 14:37:13 +0200

    fix: faulty import

--- src/routes/api/footprint/event/+server.ts
+++ src/routes/api/footprint/event/+server.ts
@@ -1,4 +1,4 @@
-import { pirsch } from '$lib/server/pirsch';
+import { pirsch } from '$lib/services/pirsch/pirsch.server';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = (e) => pirsch.event(e);
