ede8530e87a8b566d7556a355b8e8d9aee4a69ab
Jaeden <ch.schwartze@gmail.com>
2023-08-29 14:44:41 +0200

    fix: deploy issue

--- src/routes/api/footprint/event/+server.ts
+++ src/routes/api/footprint/event/+server.ts
@@ -1,4 +1,4 @@
-import { pirsch } from '$lib/services/pirsch/pirsch.server';
+import { pirsch } from '$lib/server/pirsch';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = (e) => pirsch.event(e);
